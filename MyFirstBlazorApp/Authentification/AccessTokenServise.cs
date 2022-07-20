58253bc47b7c17f490828058498c9d204a14bc3f
Emilis Vadopalas <Emeilaszx@gmail.com>
2022-07-20 18:05:58 +0300

    ready for implementation

--- MyFirstBlazorApp/Authentification/AccessTokenServise.cs
+++ MyFirstBlazorApp/Authentification/AccessTokenServise.cs
@@ -4,6 +4,56 @@
     {
         private const int TokenIsValidFor = 8;
 
-        private readonly HmacSha256
+        private readonly CryptographyServise _cryptoServise;
+
+        public AccessTokenServise(CryptographyServise cryptoServise)
+        {
+            _cryptoServise = cryptoServise;
+        }
+
+        public string CreateAccessToken(string name)
+        {
+
+        }
+
+        public string GetName(string token)
+        {
+
+        }
+
+        public string GetRole(string token)
+        {
+
+        }
+
+        private string DecodeBody(string token)
+        {
+
+        }
+
+        private string ToBase64()
+        {
+
+        }
+
+        private string FromBase64()
+        {
+
+        }
+
+        private bool IsTokenValid(string token)
+        {
+
+        }
+
+        private bool IsSignatureUntampered(string token)
+        {
+
+        }
+
+        private bool IsTokenExpired(string token)
+        {
+
+        }
     }
 }
---
+++ MyFirstBlazorApp/Authentification/UserServise.cs
@@ -0,0 +1,17 @@
+﻿namespace MyFirstBlazorApp.Authentification
+{
+    public class UserServise
+    {
+        //get user from db
+
+        public string GetHash(string input)
+        {
+
+        }
+
+        public string VerifyLogin(string login, string password)
+        {
+
+        }
+    }
+}
