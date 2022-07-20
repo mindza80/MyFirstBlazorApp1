881dc975c46e2be603d28de6911296f3bf4d7bfd
Emilis Vadopalas <Emeilaszx@gmail.com>
2022-07-20 20:39:46 +0300

    Started Token Servise

--- MyFirstBlazorApp/Authentification/AccessTokenServise.cs
+++ MyFirstBlazorApp/Authentification/AccessTokenServise.cs
@@ -1,4 +1,7 @@
-﻿namespace MyFirstBlazorApp.Authentification
+﻿using System.Text;
+using System.Text.Json;
+
+namespace MyFirstBlazorApp.Authentification
 {
     public class AccessTokenServise
     {
@@ -11,9 +14,25 @@
             _cryptoServise = cryptoServise;
         }
 
-        public string CreateAccessToken(string name)
+        public string CreateAccessToken(string name, string role)
         {
+            var header = ToBase64(JsonSerializer.Serialize(new
+            {
+                alg = "HS256",
+                typ = "JWT"
+            }));
 
+            var body = ToBase64(JsonSerializer.Serialize(new
+            {
+                iss = "MyFirstTokenServise",
+                exp = DateTime.Now.AddHours(TokenIsValidFor).ToString("yyyy-MM-dd HH:mm:ss"),
+                usr = name,
+                rol = role
+            }));
+
+            var signature = _cryptoServise.Hash($"{header}.{body}");
+
+            return $"{header}.{body}.{signature}";
         }
 
         public string GetName(string token)
@@ -31,15 +50,9 @@
 
         }
 
-        private string ToBase64()
-        {
+        private string ToBase64(string message) => Convert.ToBase64String(Encoding.UTF8.GetBytes(message));
 
-        }
-
-        private string FromBase64()
-        {
-
-        }
+        private string FromBase64(string base64Message) => Encoding.UTF8.GetString(Convert.FromBase64String(base64Message));
 
         private bool IsTokenValid(string token)
         {
