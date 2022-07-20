84940fac3f6633fccd4ba9f9b5f3c2998b84027a
Emilis Vadopalas <Emeilaszx@gmail.com>
2022-07-20 17:28:39 +0300

    Add project files.

---
+++ MyFirstBlazorApp/Authentification/AuthentificationStateProviderServise.cs
@@ -0,0 +1,22 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace MyFirstBlazorApp.Authentification
+{
+    public class AuthenticationStateProviderServise : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorageService;
+
+        public AuthenticationStateProviderServise(ILocalStorageService localStorage)
+        {
+            _localStorageService = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = _localStorageService.GetItemAsync<string>("MyFirstToken");
+
+
+        }
+    }
+}
