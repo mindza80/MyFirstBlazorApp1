eb00a39a88a1f79b6a68c1b6f3a068e5f4c42c38
Emilis Vadopalas <Emeilaszx@gmail.com>
2022-07-20 17:54:13 +0300

    Added Criptography Service (HMAC.SHA256)

---
+++ MyFirstBlazorApp/Authentification/AccessTokenServise.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFirstBlazorApp.Authentification
+{
+    public class AccessTokenServise
+    {
+        private const int TokenIsValidFor = 8;
+
+        private readonly HmacSha256
+    }
+}
---
+++ MyFirstBlazorApp/Authentification/CryptographyServise.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MyFirstBlazorApp.Authentification
+{
+    public class CryptographyServise
+    {
+        private readonly string _secret;
+
+        public CryptographyServise(string secret)
+        {
+            _secret = secret;
+        }
+
+        public string Hash(string message)
+        {
+            var encoding = new ASCIIEncoding();
+            byte[] keyBytes = encoding.GetBytes(_secret);
+            byte[] messageBytes = encoding.GetBytes(message);
+
+            var hMacSha256Provider = new HMACSHA256(keyBytes);
+            byte[] hashedMessage = hMacSha256Provider.ComputeHash(messageBytes);
+
+            return Convert.ToBase64String(hashedMessage);
+        }
+
+        public bool VerifyHash(string hash, string message) => Hash(message) == hash;
+    }
+}
